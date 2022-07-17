1601264aa9fa90c0763f0b583cc9d88c06372a2f
lujainE <106912035+lujainE@users.noreply.github.com>
2022-07-17 10:20:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function searchFunction() {
+    var input;
+    input = document.getElementById("searchInput");
+}
+
+function fontchange(i){
+    var paragraph = document.getElementById('para');
+    paragraph.style.fontSize = i + "px";
+}
